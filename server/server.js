c093ce65bed907a5e4dd1a40b9c6a59d43aeccce
aziz <ahmed613@umn.edu>
2022-02-17 18:30:08 -0600

    add mutations for friends, reactions, and thoughts

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 //import ApolloServer
 const { ApolloServer} = require('apollo-server-express')
+const { authMiddleware } = require('./utils/auth');
 
 //import our typeDefs and resolvers
 const { typeDefs, resolvers} = require('./schemas')
@@ -14,9 +15,7 @@ const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    /*context: ({ req }) => ({
-      data: authMiddleware(req)
-    })*/
+    context: authMiddleware
   })
 
   // start the apollo server
