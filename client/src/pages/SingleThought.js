b56075e909c5634a0d0cec83f97fa73697e49c2c
aziz <ahmed613@umn.edu>
2022-02-21 16:37:00 -0600

    adding singlethought,reactions, profile, and friendslist

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHT } from '../utils/queries';
+import ReactionList from '../components/ReactionList';
 
 const SingleThought = props => {
+  const { id: thoughtId } = useParams();
+  console.log(thoughtId);
+
+  const { loading, data } = useQuery(QUERY_THOUGHT, {
+    variables: { id: thoughtId }
+  });
+  
+  const thought = data?.thought || {};
+  
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
     <div>
       <div className="card mb-3">
@@ -11,9 +27,10 @@ const SingleThought = props => {
           thought on createdAt
         </p>
         <div className="card-body">
-          <p>Thought Text</p>
+          <p>T{thought.thoughtText}</p>
         </div>
       </div>
+      {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
     </div>
   );
 };
